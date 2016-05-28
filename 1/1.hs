6b33f34d1c8ee3a5d5e0395fd2a2835b5c36b0cb
Nick Johnstone <ncwjohnstone@gmail.com>
2016-05-28 18:59:47 +1200

    Handle second part of day 1 challenge

--- 1/1.hs
+++ 1/1.hs
@@ -1,9 +1,20 @@
 main = do
   contents <- readFile "input"
-  print $ sum $ map parseChar contents
+  print $ floorsTravelledUntilBasementEntered contents
+
+floorsTravelledUntilBasementEntered movements =
+  length
+  $ takeWhile aboveBasement
+  $ traverseLevels
+  $ map parseChar movements
+
+add a b = a + b
+
+aboveBasement level = 0 <= level
+
+traverseLevels levels = scanl add 0 levels
 
 parseChar char =
   case char of
     '(' -> 1
     ')' -> -1
-    _ -> 0
